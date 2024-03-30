fd5cbef849f6090180f186e4288ff8e354692963
NatashaLC <94565755+Natashalopez05@users.noreply.github.com>
2024-03-30 00:05:49 -0400

    Update tarea_8.hs

--- tarea_8.hs
+++ tarea_8.hs
@@ -1,35 +1,43 @@
 import qualified Data.Map.Strict as Map
+import Text.Read (readMaybe)
 
 -- Definición de tipos de datos para expresiones aritméticas
 data Expr = Var String
           | Val Int
-          | Add Expr Expr
-          | Sub Expr Expr
-          | Mul Expr Expr
-          | Div Expr Expr
+          | BinOp BinOperator Expr Expr
           deriving (Show)
+
+data BinOperator = Add | Sub | Mul | Div deriving (Show)
 
 -- Definición de tipos de datos para comandos (asignación y expresión)
 data Command = Assign String Expr
              | ExprCmd Expr
+             | Quit
              deriving (Show)
 
 -- Definición de un entorno que mapea nombres de variables a sus valores
 type Env = Map.Map String Int
 
 -- Evaluación de expresiones
-eval :: Env -> Expr -> Int
-eval _ (Val n) = n
-eval env (Var x) = Map.findWithDefault (error $ "Variable no definida: " ++ x) x env
-eval env (Add e1 e2) = eval env e1 + eval env e2
-eval env (Sub e1 e2) = eval env e1 - eval env e2
-eval env (Mul e1 e2) = eval env e1 * eval env e2
-eval env (Div e1 e2) = eval env e1 `div` eval env e2
+eval :: Env -> Expr -> Maybe Int
+eval _ (Val n) = Just n
+eval env (Var x) = Map.lookup x env
+eval env (BinOp op e1 e2) = do
+    v1 <- eval env e1
+    v2 <- eval env e2
+    case op of
+        Add -> pure (v1 + v2)
+        Sub -> pure (v1 - v2)
+        Mul -> pure (v1 * v2)
+        Div -> if v2 /= 0 then Just (v1 `div` v2) else Nothing
 
--- Ejecución de comandos (asignación o expresión)
+-- Ejecución de comandos (asignación, expresión o salir)
 execute :: Env -> Command -> (Env, Maybe Int)
-execute env (Assign var expr) = (Map.insert var (eval env expr) env, Nothing)
-execute env (ExprCmd expr) = (env, Just (eval env expr))
+execute env (Assign var expr) = case eval env expr of
+                                    Just val -> (Map.insert var val env, Nothing)
+                                    Nothing -> (env, Just $ error "Error de evaluación")
+execute env (ExprCmd expr) = (env, eval env expr)
+execute _ Quit = (Map.empty, Nothing)
 
 -- Función principal del REPL
 repl :: Env -> IO ()
@@ -37,32 +45,35 @@ repl env = do
     putStr "> "
     input <- getLine
     let (newEnv, result) = case parseCommand input of
-                             Just cmd -> execute env cmd
-                             Nothing -> (env, Just (error "Error de sintaxis"))
+                                Just cmd -> execute env cmd
+                                Nothing -> (env, Just $ error "Error de sintaxis")
     case result of
-      Just val -> putStrLn $ show val
-      Nothing -> return ()
-    repl newEnv
+        Just val -> putStrLn $ show val
+        Nothing -> return ()
+    if result /= Just "quit" then repl newEnv else return ()
 
 -- Analizador sintáctico de comandos
 parseCommand :: String -> Maybe Command
-parseCommand str =
-    case words str of
-      [var, "=", expr] -> Just $ Assign var (parseExpr expr)
-      _ -> Just $ ExprCmd (parseExpr str)
+parseCommand str
+    | str == "quit" = Just Quit
+    | length parts == 3 && parts !! 1 == "=" = do
+        let var = parts !! 0
+        expr <- parseExpr (unwords $ drop 2 parts)
+        Just $ Assign var expr
+    | otherwise = ExprCmd <$> parseExpr str
+    where parts = words str
 
 -- Analizador sintáctico de expresiones aritméticas
-parseExpr :: String -> Expr
-parseExpr = parse . words
-  where
-    parse [x] = if all (`elem` ['0'..'9']) x
-                then Val (read x)
-                else Var x
-    parse (x:"+":y:rest) = Add (parseExpr x) (parseExpr y)
-    parse (x:"-":y:rest) = Sub (parseExpr x) (parseExpr y)
-    parse (x:"*":y:rest) = Mul (parseExpr x) (parseExpr y)
-    parse (x:"/":y:rest) = Div (parseExpr x) (parseExpr y)
-    parse _ = error "Expresión inválida"
+parseExpr :: String -> Maybe Expr
+parseExpr str = case words str of
+    [x] -> Var <$> Just x
+    [x] -> Val <$> readMaybe x
+    (x:op:y:rest) -> case op of
+                        "+" -> BinOp Add <$> parseExpr x <*> parseExpr (unwords (y:rest))
+                        "-" -> BinOp Sub <$> parseExpr x <*> parseExpr (unwords (y:rest))
+                        "*" -> BinOp Mul <$> parseExpr x <*> parseExpr (unwords (y:rest))
+                        "/" -> BinOp Div <$> parseExpr x <*> parseExpr (unwords (y:rest))
+    _ -> Nothing
 
 -- Función principal
 main :: IO ()
